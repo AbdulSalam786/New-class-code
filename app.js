48489d13c08b3ad1ef90810806b579bfd7332b2e
unknown <salmankhan7778666@gmail.com>
2018-03-11 19:44:29 +0500

    Stop watch

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+var min = 0;
+var sec = 0;
+var millsec = 0;
+
+var timer;
+
+function calltimer(){
+    millsec++
+    if (millsec < 100) {
+        if (millsec === 99){
+            millsec = 0;
+            sec++ 
+            if (sec === 60 ){
+                sec = 0;
+                min++
+            }
+        }
+    }
+    else {
+        millsec = 0;
+    }
+    document.getElementById('timer').innerHTML = min + ":" + sec + ":" + millsec;
+}
+
+function start(){
+    timer = setInterval(calltimer,9)
+}
+
+function stop(){
+    clearInterval(timer)
+}
+
+function reset(){
+    clearInterval(timer)
+    document.getElementById("timer").innerHTML = 0 + ":" + 0;
+    millsec = 0;
+    sec = 0;
+    min = 0;
+}
